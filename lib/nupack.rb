e534463f5b6ecf27c68426be36085dae2355a91a
Harman Singh <harman.j.singh@hotmail.com>
2016-10-20 03:30:49 -0500

    Passed all the electronics tests

--- lib/nupack.rb
+++ lib/nupack.rb
@@ -3,9 +3,10 @@ FLATRATE = 0.05
 PERSONRATE = 0.012
 DRUGSRATE = 0.075
 FOODRATE = 0.13
+ELECTRONICSRATE = 0.02
 
     def flatMarkupPrice(price)
-        return (price*(1+FLATRATE)).round(2)
+        return (price*(1+FLATRATE))
     end
 
     def projectCost(price, people, type)
@@ -14,6 +15,8 @@ FOODRATE = 0.13
             return (flatMarkupPrice(price)*(1+ people*PERSONRATE + DRUGSRATE)).round(2)
         elsif(type == "food")
             return (flatMarkupPrice(price)*(1+ people*PERSONRATE + FOODRATE)).round(2)
+        elsif(type == "electronics")
+            return (flatMarkupPrice(price)*(1+ people*PERSONRATE + ELECTRONICSRATE)).round(2)
         end
     end
 
