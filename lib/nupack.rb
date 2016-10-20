e4fc1b56799a2716d1ab374684295db33d7ba5dd
Harman Singh <harman.j.singh@hotmail.com>
2016-10-20 03:06:31 -0500

    Implemented functionality to pass all tests for projectCost for drugs

--- lib/nupack.rb
+++ lib/nupack.rb
@@ -1,8 +1,17 @@
 class Nupack
 FLATRATE = 0.05
+PERSONRATE = 0.012
+DRUGSRATE = 0.075
 
     def flatMarkupPrice(price)
         return (price*(1+FLATRATE)).round(2)
+    end
+
+    def projectCost(price, people, type)
+
+        if(type == "drugs")
+            return (flatMarkupPrice(price)*(1+ people*PERSONRATE + DRUGSRATE)).round(2)
+        end
     end
 
 
