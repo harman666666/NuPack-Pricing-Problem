4da2a7bc29cf2fb9c4392638be6f3256776d6188
Harman Singh <harman.j.singh@hotmail.com>
2016-10-20 03:17:10 -0500

    Implemented and passed the Food tests

--- lib/nupack.rb
+++ lib/nupack.rb
@@ -2,6 +2,7 @@ class Nupack
 FLATRATE = 0.05
 PERSONRATE = 0.012
 DRUGSRATE = 0.075
+FOODRATE = 0.13
 
     def flatMarkupPrice(price)
         return (price*(1+FLATRATE)).round(2)
@@ -11,6 +12,8 @@ DRUGSRATE = 0.075
 
         if(type == "drugs")
             return (flatMarkupPrice(price)*(1+ people*PERSONRATE + DRUGSRATE)).round(2)
+        elsif(type == "food")
+            return (flatMarkupPrice(price)*(1+ people*PERSONRATE + FOODRATE)).round(2)
         end
     end
 
